cbcd45a8864824853e5f49cc47cc5c6317fc62e8
glennex <siddiquiamaan13@gmail.com>
2024-03-06 14:43:32 +0530

    AM- created trending coins section

--- src/componenets/content/Content.jsx
+++ src/componenets/content/Content.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
+import TrendinCoins from '../trendingCoins/TrendinCoins';
 import "./content.css";
+import startedImg from '../../assets/temp.png'
+import { FaArrowRightLong } from "react-icons/fa6";
+import { RxDoubleArrowRight } from "react-icons/rx";
 
 function Content() {
   return (
     <div className='content-container'>
-         <p><span className='content-descript'>Cryptocurrencies  >></span> Bitcoin</p>
+         <p><span className='content-descript'>Cryptocurrencies  <RxDoubleArrowRight/></span> Bitcoin</p>
          <div className="left-right-wrapper">
          <div className="left-container">
        <div className="BTC-card"></div>
        </div>
        <div className="right-container">
         <div className="getStarted-card">
-            <h3>Get Started with KoinX for FREE</h3>
-            <p>With our range of features that ypu can equip for free, KoinX allows you to be more educated and aware of your tax reports.</p>
+          <div><h2>Get Started with KoinX</h2>
+            <h2> for FREE</h2></div>
+            <p>With our range of features that you can equip for free, KoinX allows you to be more educated and aware of your tax reports.</p>
+            <img src={startedImg}/>
+            <button> Get Started for FREE <FaArrowRightLong/></button>
         </div>
+        <TrendinCoins/>
+
        </div>
          </div>
        
---
+++ src/componenets/trendingCoins/TrendinCoins.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { useState, useEffect } from 'react';
+import { BiSolidUpArrow } from "react-icons/bi";
+import { BiSolidDownArrow } from "react-icons/bi";
+import './trendingCoins.css'
+
+function TrendinCoins() {
+    const [topCoins, setTopCoins] = useState([]);
+
+    useEffect(() => {
+      const fetchTopCoins = async () => {
+        try {
+          const response = await fetch("https://api.coingecko.com/api/v3/search/trending");
+          const data = await response.json();
+          console.log(data.coins);
+          const topThreeCoins = data.coins.slice(0, 3).map(coin => {
+            const priceChangeINR = parseFloat(coin.item.data.price_change_percentage_24h.inr).toFixed(2);
+            return {
+              name: coin.item.name,
+              imageUrl: coin.item.small,
+              symbol:coin.item.symbol,
+              priceChangeINR: priceChangeINR
+            };
+          });
+          setTopCoins(topThreeCoins);
+          console.log(topThreeCoins);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+  
+      fetchTopCoins();
+    }, []);
+
+  return (
+    <div>
+      <div className="trending-card">
+        <h3>Trending Coins (24h)</h3>
+        {/* <div className="trending-coins-list"> */}
+          
+        {topCoins.map((coin, index) => (
+          <div className="trending-coins-list" key={index}>
+            <div className="left-grp-trending">
+            <img src={coin.imageUrl} alt={coin.name} />
+            <p>{coin.name}({coin.symbol})</p>
+            </div>
+            
+            <div className={`priceChange-indicator ${coin.priceChangeINR > 0 ? "positive-priceChange": "negative-priceChange" } `}> 
+            <p>{coin.priceChangeINR > 0 ? <BiSolidUpArrow /> : <BiSolidDownArrow/>}{coin.priceChangeINR}%</p>
+            </div>
+            
+          </div>
+        ))}
+     
+        {/* </div> */}
+      </div>
+    </div>
+  )
+}
+
+export default TrendinCoins
